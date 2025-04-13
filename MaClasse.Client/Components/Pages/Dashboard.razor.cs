34df6bde8550e2d6e6a185171e8dc057744ddc32
Baptiste BOIMARD <94848384+baptiste-boimard@users.noreply.github.com>
2025-04-13 17:45:07 +0200

    début du dashboard

---
+++ MaClasse.Client/Components/Pages/Dashboard.razor.cs
@@ -0,0 +1,45 @@
+﻿using MaClasse.Client.States;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace MaClasse.Client.Components.Pages;
+
+public partial class Dashboard : ComponentBase
+{
+    private readonly UserState _userState;
+    private readonly AuthenticationStateProvider _authenticationStateProvider;
+
+    public Dashboard(
+        UserState userState,
+        AuthenticationStateProvider authenticationStateProvider)
+    {
+        _userState = userState;
+        _authenticationStateProvider = authenticationStateProvider;
+    }
+    
+    private UserState? userInformation;
+
+    protected override async Task OnInitializedAsync()
+    {
+        _userState.OnChange += HandleUserStateChanged;
+
+        var authState = await _authenticationStateProvider.GetAuthenticationStateAsync();
+        var user = authState.User;
+
+        if (user.Identity is { IsAuthenticated: true })
+        {
+            userInformation = _userState.GetUser();
+        }
+    }
+
+    private void HandleUserStateChanged()
+    {
+        userInformation = _userState.GetUser();
+        InvokeAsync(StateHasChanged);
+    }
+
+    public void Dispose()
+    {
+        _userState.OnChange -= HandleUserStateChanged;
+    }
+}
