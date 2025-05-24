6d7d992da5a9bdcf6a7bdfef09ff66b604cddb27
Baptiste BOIMARD <94848384+baptiste-boimard@users.noreply.github.com>
2025-05-24 15:15:44 +0200

    Modif du workflow 4eme

--- Service.OAuth/Controller/AuthController.cs
+++ Service.OAuth/Controller/AuthController.cs
@@ -51,7 +51,7 @@ public class AuthController: ControllerBase
     [Route("google-login")]
     public async Task<IActionResult> GoogleLogin(GoogleTokenRequest request)
     {
-        Console.WriteLine("coucoffu");
+        Console.WriteLine("coucoeeffu");
         var payload = await _validateGoogleTokenService.ValidateGoogleToken(request.Token);
         if (payload == null)
         {
