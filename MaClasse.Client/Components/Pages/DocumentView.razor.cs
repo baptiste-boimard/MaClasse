a07cf28237fdda12ffca38d4f2636022ecc1cbec
Baptiste BOIMARD <94848384+baptiste-boimard@users.noreply.github.com>
2025-06-13 18:28:37 +0200

    Fix: Essai de mise à jour des container pour le nouveau docker compose v4

--- MaClasse.Client/Components/Pages/DocumentView.razor.cs
+++ MaClasse.Client/Components/Pages/DocumentView.razor.cs
@@ -19,7 +19,7 @@ public partial class DocumentView : ComponentBase
     _jsRuntime = jsRuntime;
   }
   
-  // essais 3
+  // essais 4
   private static readonly string[] ImageFormats = { "png", "jpg", "jpeg", "bmp", "gif", "webp", "image/png", "image/jpeg" };
 
   private bool IsImage(string format)
