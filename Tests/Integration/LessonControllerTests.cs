afd03f7bedd6ae1e21b7948b00d762c39b970371
Baptiste BOIMARD <94848384+baptiste-boimard@users.noreply.github.com>
2025-06-14 19:52:51 +0200

    Improve tests

--- Tests/Integration/LessonControllerTests.cs
+++ Tests/Integration/LessonControllerTests.cs
@@ -99,63 +99,29 @@ public class LessonControllerTests : IClassFixture<WebApplicationFactory<Service
         var resp = await client.PostAsJsonAsync("/api/add-lesson", req);
         Assert.Equal(HttpStatusCode.BadRequest, resp.StatusCode);
     }
-}
 
-=======
-using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.Extensions.DependencyInjection;
-using System.Linq;
-using System.Net.Http;
-using System.Threading.Tasks;
-using Moq;
-using Service.Database.Interfaces;
-using MaClasse.Shared.Models.Files;
-using Microsoft.Extensions.Configuration;
-using Service.Database.Services;
-using Xunit;
-
-namespace Tests.Integration
-{
-    public class LessonControllerTests : IClassFixture<WebApplicationFactory<Service.Database.Program>>
+    [Fact]
+    public async Task DeleteLesson_ReturnsNotFound_WhenMissing()
     {
-        private readonly WebApplicationFactory<Service.Database.Program> _factory;
-        private readonly Mock<ILessonRepository> _repoMock = new();
+        _repoMock.Setup(r => r.GetLesson("A", "1")).ReturnsAsync((Lesson?)null);
+        var client = _factory.CreateClient();
+        var req = new RequestLesson { IdSession = "s", IdAppointement = "A", Lesson = new Lesson() };
+        var resp = await client.PostAsJsonAsync("/api/delete-lesson", req);
+        Assert.Equal(HttpStatusCode.NotFound, resp.StatusCode);
+    }
 
-        public LessonControllerTests(WebApplicationFactory<Service.Database.Program> factory)
-        {
-            _factory = factory.WithWebHostBuilder(builder =>
-            {
-                builder.ConfigureServices(services =>
-                {
-                    var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(ILessonRepository));
-                    if (descriptor != null) services.Remove(descriptor);
-                    services.AddSingleton(_repoMock.Object);
-                    // ensure UserService can be resolved even if not used
-                    services.AddSingleton<UserService>(new UserService(new HttpClient(), new ConfigurationBuilder().Build()));                });
-            });
-        }
-
-        [Fact]
-        public async Task GetDocument_ReturnsNotFound_WhenMissing()
-        {
-            _repoMock.Setup(r => r.GetDocument(It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync((Document?)null);
-            var client = _factory.CreateClient();
-            var request = new FileRequestToDatabase { IdUser = "1", Document = new Document { IdDocument = "42" } };
-            var response = await client.PostAsJsonAsync("/api/get-document", request);
-            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
-        }
-
-        [Fact]
-        public async Task GetDocument_ReturnsDocument_WhenExists()
-        {
-            var doc = new Document { IdDocument = "42", Name = "Test" };
-            _repoMock.Setup(r => r.GetDocument("42", "1")).ReturnsAsync(doc);
-            var client = _factory.CreateClient();
-            var request = new FileRequestToDatabase { IdUser = "1", Document = new Document { IdDocument = "42" } };
-            var response = await client.PostAsJsonAsync("/api/get-document", request);
-            response.EnsureSuccessStatusCode();
-            var returned = await response.Content.ReadFromJsonAsync<Document>();
-            Assert.Equal("Test", returned?.Name);
-        }
+    [Fact]
+    public async Task DeleteLesson_Deletes_WhenFound()
+    {
+        var lesson = new Lesson { IdLesson = "L", IdAppointment = "A" };
+        _repoMock.Setup(r => r.GetLesson("A", "1")).ReturnsAsync(lesson);
+        _repoMock.Setup(r => r.DeleteLesson(lesson, "1")).ReturnsAsync(lesson);
+        var client = _factory.CreateClient();
+        var req = new RequestLesson { IdSession = "s", IdAppointement = "A", Lesson = lesson };
+        var resp = await client.PostAsJsonAsync("/api/delete-lesson", req);
+        resp.EnsureSuccessStatusCode();
+        _repoMock.Verify(r => r.DeleteLesson(lesson, "1"), Times.Once);
     }
 }
+
+
